3c4aef19cc36fc7faf5bb2bc2e6eba8b43d3d89c
PaulinoAranda <36703215+PaulinoAranda@users.noreply.github.com>
2022-04-05 14:05:54 +0200

    Update feature.cpp

--- src/pal/feature.cpp
+++ src/pal/feature.cpp
@@ -164,7 +164,7 @@ int Feature::setPositionForPoint(double x, double y, double scale,
 			scale, delta_width);
 
 	int nbp = layer->pal->point_p;
-	int distanceNbp = 4;
+	int distanceNbp = 3;
 //std::cout << "Nbp : " << nbp << std::endl;
 
 	int i, ii;
